abf196790d0b65fa6fb559a69ba7839079b2b3ba
SLICKMAN-TYRUS <a.chol@alustudent.com>
2026-01-24 17:44:11 +0300

    Add tasks 1-2: trim matrix columns and calculate matrix shape

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for calculating matrix shape"""
+
+
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix
+
+    Args:
+        matrix: A matrix (list of lists) of any dimension
+
+    Returns:
+        A list of integers representing the shape of the matrix
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
